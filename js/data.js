0b4ac5e68cf7a2b3e1ef2aacc1b9b0b451327177
julianvanstaden <125472051+julianvanstaden@users.noreply.github.com>
2023-06-19 09:38:53 +0200

    last commit

---
+++ js/data.js
@@ -0,0 +1,15 @@
+let subData = [
+    {
+        subName: "The Captain's Catch",
+        bread: "Whole Wheat",
+        subToppings: ["bacon", "ham", "musshroom"],
+        subPrice: 80,
+    },
+    {
+        subName: "The Veggie Voyage",
+        bread: "Whole Wheat",
+        subToppings: ["lettuce", "tomato", "olive"],
+        subPrice: 70,
+    },
+
+]
--- js/index.js
+++ js/index.js
@@ -1,31 +1,94 @@
-   // Add event listener to the form
-   document.getElementById('subForm').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent form submission
+let subOrder = [];
 
-    // Get the selected options
-    var subName = document.getElementById('subName').value;
-    var bread = document.getElementById('bread').value;
-    var toppings = getSelectedCheckboxes('toppings');
-    var sauces = getSelectedCheckboxes('sauce');
+let subName = document.getElementById("subName").value;
+let bread = document.getElementById("bread").value;
 
-    // Calculate the subtotal
-    var subCost = calculateSubtotal(toppings.length, sauces.length);
+if(bread === "Italian"){
+    subTotal = subTotal + 20;
+} else if(bread === "Whole Wheat"){
+    subTotal = subTotal + 40;
+} else if(bread === "Multigrain"){
+    subTotal = subTotal + 60;
+}
 
-    // Display the subtotal
-    document.getElementById('subCost').value = subCost;
+let toppingOptions = document.getElementsByName("toppings");
+let topArray = [];
+for(let i = 0; i < toppingOptions.length; i++){
+    if(toppingOptions[i].checked){
+        topArray.push(toppingOptions[i].value);
+        subTotal = subTotal + +toppingOptions[i].dataset.cost
+    }
+}
 
-    // Add the sub to the checkout or perform any desired action with the selected items
+subOrder.push({
+    subName: subName,
+    bread: bread,
+    subToppings: topArray,
+    subPrice: subTotal
+});
 
-    // Reset the form
-    document.getElementById('subForm').reset();
-  });
+console.log(subOrder)
 
-  // Helper function to get selected checkboxes
-  function getSelectedCheckboxes(checkboxName) {
-    var checkboxes = document.getElementsByName(checkboxName);
-    var selectedCheckboxes = [];
+document.getElementById("realTimeCost").innerHTML = "R0.00"
+document.getElementById("subForm").reset();
 
-    for (var i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].checked) {
-        selectedCheckboxes
-  
+
+
+realTimeCost = () => {
+
+realTimePrice = 0; 
+
+let bread = document.getElementById("bread").value;
+if(bread === "Italian"){
+    realTimePrice = realTimePrice + 20;
+} else if(bread === "Whole Wheat"){
+    realTimePrice = realTimePrice + 40;
+} else if(bread === "Multigrain"){
+    realTimePrice = realTimePrice + 60;
+}
+
+
+
+let toppingOptions = document.getElementsByName("toppings");
+for(let i = 0; i < toppingOptions.length; i++){
+    if(toppingOptions[i].checked){
+        realTimePrice = realTimePrice + +toppingOptions[i].dataset.cost
+    }
+}
+
+document.getElementById("realTimeCost").innerHTML = "R" + realTimePrice + ".00"
+
+}
+
+displayOrder = () => {
+
+let area = document.getElementById("orders");
+let total = document.getElementById("orderTotal");
+
+area.innerHTML = "";
+
+let overallTotal = 0; 
+
+for(let i = 0; i < subOrder.length; i++){
+
+    let name = subOrder[i].subName;
+    let bread = subOrder[i].bread;
+    let toppings = subOrder[i].toppingOptions;
+    let price = subOrder[i].subPrice; 
+
+    overallTotal += price;
+
+    area.innerHTML += `
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${name}</h5>
+                    <p class="card-text"><strong>bread:</strong> ${bread}</p>
+                    <p class="card-text"><strong>Toppings:</strong> ${toppings}</p>
+                    <p class="card-text"><strong>Cost:</strong> R${price}.00</p>
+                </div>
+            </div>`
+
+    total.innerHTML = "R" + overallTotal + ".00"
+
+}
+}
